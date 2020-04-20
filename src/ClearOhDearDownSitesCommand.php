479bbdf431f1ce8e6b8d54fb2db3accb416987b8
freek <freek@spatie.be>
2020-04-20 19:36:19 +0200

    wip

---
+++ src/ClearOhDearDownSitesCommand.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Tiles\Uptime;
+
+use Illuminate\Console\Command;
+
+class ClearOhDearDownSitesCommand extends Command
+{
+    protected $signature = 'dashboard:clear-oh-dear-down-sites';
+
+    protected $description = 'Clear all sites that are down';
+
+    public function handle()
+    {
+        $this->info('Clearing down sites...');
+
+        OhDearUptimeStore::make()->clearDownSites();
+
+        $this->info('All done!');
+    }
+}
