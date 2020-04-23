e1c4df4bcaaabb73933bb49021293e91df7d400b
freek <freek@spatie.be>
2020-04-23 21:43:42 +0200

    wip

--- resources/views/tile.blade.php
+++ resources/views/tile.blade.php
@@ -1,8 +1,14 @@
-<x-dashboard-tile :position="$position" :show="$showTile" :fade="false">
+<x-dashboard-tile
+    :position="$position"
+    :show="$showTile"
+    :fade="false"
+    :refresh-interval="$refreshIntervalInSeconds"
+>
     <div class="absolute inset-0 bg-warning p-4">
-        <div wire:poll.{{ $refreshIntervalInSeconds }}s class="grid grid-rows-auto-1 gap-2 h-full">
+        <div class="grid grid-rows-auto-1 gap-2 h-full">
             <h1 class="uppercase font-bold">Downtime</h1>
             <ul class="self-center divide-y-2">
+
                 @foreach($downSites as $downSite)
                     <li
                         class="py-1 font-semibold truncate"
